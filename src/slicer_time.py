f7d25d11153f70ec38ced5a70a546af5873ace61
sd <leowest1921@gmail.com>
2023-09-16 13:18:41 +0800

    add

--- src/slicer_time.py
+++ src/slicer_time.py
@@ -16,8 +16,11 @@ def slicer_time(file: str, time_unit:int=20000):
     cuts = make_chunks(audio, size)  #将文件切割为10s一块
     print(f"====chunks={cuts}")
     for i, seg in enumerate(cuts):
+        chunk = seg.raw_data
+        if chunk is None or "shape" not in chunk or len(chunk) < 1 :
+            continue
         #chunk_name = "chunk{0}.wav".format(i)
-        print ("exporting", i)
+        print ("exporting", i, seg.raw_data.shape)
         #chunk.export(chunk_name, format="wav")
         chunks.append(seg.raw_data)
     return chunks
