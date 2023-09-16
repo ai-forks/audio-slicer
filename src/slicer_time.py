9769b122ff35e7d3d539fb238f2a9f4ce9023bcb
sd <leowest1921@gmail.com>
2023-09-16 11:50:13 +0800

    add

--- src/slicer_time.py
+++ src/slicer_time.py
@@ -5,10 +5,11 @@ import soundfile  # Optional. Use any library you like to write audio files.
 # 按时间切换, 默认20s
 def slicer_time(file: str, time_unit:int=20000):
     file = file.replace("\\", "/")
-    #names = ext = re.split("/", file)[-1]
+    names = ext = re.split("/", file)[-1]
     #name = names.split(".")[0]
-    #ext = names.split(".")[1]
-    #ext = ext if ext is not None and len(ext) > 0 else "wav"
+    ext = names.split(".")[1]
+    ext = ext if ext is not None and len(ext) > 0 else "wav"
+    print(f"slicer_time file={file} ext={ext}")
     audio = AudioSegment.from_file(file, ext)
     size = time_unit   #切割的毫秒数 10s=10000
     chunks = make_chunks(audio, size)  #将文件切割为10s一块
